75f327722ff9bd1379ed8852201a95f895e60013
Jeanette-Mosqueda <70932583+Jeanette-Mosqueda@users.noreply.github.com>
2021-02-18 00:51:35 -0500

    add playground

---
+++ ActididadTres_LJPM/actividadTres_LJPM.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var numI:Int = 9
+print(numI)
+var numE = 5
+print(numE)
+var numD = 2.5
+print (numD)
+var cadena = "Trabajo de Leslie"
+print(cadena)
+
+var punto1  = (3.3)
+print(punto1)
+var numA:Int = 32
+print(numA)
+var cadA:String = "asociacion"
+print(cadA)
+
+var numeros:Array<Int> = Array<Int>()
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+numeros.append(4)
+numeros.append(5)
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+print("Numeros: \(numeros)")
+
+var diasSemana:Dictionary<Int, String> =
+ Dictionary<Int, String>()
+diasSemana = [1:"Lunes"]
+diasSemana = [2:"Martes"]
+diasSemana = [3:"Miercoles"]
+diasSemana = [4:"Jueves"]
+diasSemana = [5:"Viernes"]
+diasSemana = [6:"Sabado"]
+diasSemana = [1:"Domingo"]
+print("Dias de la Semana: \(diasSemana)")
+
+
+
+
